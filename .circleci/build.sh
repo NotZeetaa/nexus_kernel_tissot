01b788ac3a12070b288bd7a5640a02f4fe3824dd
NotZeetaa <rodrigo2005contente@gmail.com>
2021-04-16 00:44:09 +0100

    circleci:update
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,7 +5,7 @@ git clone --depth=1 https://github.com/NotZeetaa/Flashable_Zip AnyKernel
 echo "Done"
 DEVICE=tissot
 DEFCONFIG=lightning-tissot_defconfig
-VERSION=V0.1
+VERSION=V0.2
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
